a5271349c30f13a2b6d329ece8799e558d600fbe
miroslav.stepanek <info@mirousek.eu>
2023-04-03 11:16:11 +0200

    exam 0001 job definition updated

--- nodejs/0001/src/job.ts
+++ nodejs/0001/src/job.ts
@@ -1,7 +1,12 @@
-import { MongoClient } from './mongoWrapper'
-import { InitMultipartUpload, UploadPart, TestMultipartUpload } from './sink'
+import { MongoClient } from '../lib/mongoWrapper'
+import { initMultipartUpload, uploadPart, completeMultipartUpload, PartDescriptor } from '../lib/sink'
 
-export async function process(shouldEnd: () => boolean): Promise<boolean> {
+export enum ProcessResult {
+    NotYetDone,
+    Finished
+}
+
+export async function process(shouldEnd: () => boolean): Promise<ProcessResult> {
     // Your code goes here
     throw Error('Not implemented')
 }
